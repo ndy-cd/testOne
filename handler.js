29b28e649344702c12aa765f1a08c0f1901106a0
nody <nody.email@gmail.com>
2019-11-14 23:54:34 +0500

    Add html and show it on the page

--- handler.js
+++ handler.js
@@ -3,15 +3,15 @@ const fs = require('fs');
 
 function _handler(req, res) {
     res.writeHead(200)
-    fs.readFile('./doc.txt', (err, data) => {
+    fs.readFile('newFolder/file.html', (err, data) => {
         if(err) {
             res.writeHead(500);
             res.end(err);
             return;
         }
         res.writeHead(200);
-        res.end('Hello fckn world\n
-                <p>OK</p>' + req.url + data)
+        // res.end('Hello fckn world\n' + req.url + data)
+        res.end(data);
         })
     // setTimeout(() => {res.end('Hello fckn world\n' + req.url)}, 5000);
     
