ce62683bbaaddefea0cb54675518ff9dba9d8bf8
nody <nody.email@gmail.com>
2019-11-18 15:47:56 +0500

    Task-4 modify handler; Show file on method GET (by default from browser); Write to file 'request.txt' on method POST

--- handler.js
+++ handler.js
@@ -12,8 +12,18 @@ function _handler(req, res) {
         }
         res.writeHead(200);
         var result = String(data).replace('Insert Here', doc);
-
-        res.end(result);
+        if (req.url != "/favicon.ico"){
+            if (req.method == "POST"){
+                //console.log('Need to post!' + req.url);
+                fs.writeFileSync('./request.txt', doc);
+                res.end();
+            }
+            if (req.method == "GET"){
+                //console.log('Need to get!' + req.url);
+                res.end(result);
+            }
+        }
+        res.end();
         })    
 }
 
