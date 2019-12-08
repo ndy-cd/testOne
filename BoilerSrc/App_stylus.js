33108a276eed6be24ab9c73a169c702a050eea55
nody <nody.email@gmail.com>
2019-12-08 16:11:43 +0500

    Stylus homework n.1, centered

---
+++ BoilerSrc/App_stylus.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import '../styles/App.styl';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="button"></div>
+        );
+    }
+}
+
+export default App;
